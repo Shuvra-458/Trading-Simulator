c21cd3178bff48803242949797e0c59cc00cb020
Shuvrajyoti <shuvra458@gmail.com>
2025-08-03 22:21:02 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -9,10 +9,10 @@ from .db_base import Base
 env_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 load_dotenv(dotenv_path=env_path)
 
-DATABASE_URL = os.getenv("DATABASE_URL", pool_pre_ping=True,
-    pool_recycle=280)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL,pool_pre_ping=True,
+    pool_recycle=280)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
