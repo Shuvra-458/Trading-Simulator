7065a6b33c7879cd264207af88beb94e716f55b3
Shuvrajyoti <shuvra458@gmail.com>
2025-08-03 22:17:22 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -9,7 +9,8 @@ from .db_base import Base
 env_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 load_dotenv(dotenv_path=env_path)
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", pool_pre_ping=True,
+    pool_recycle=280)
 
 engine = create_engine(DATABASE_URL)
 
